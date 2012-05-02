ab4b665f147d50cba8c2d15c376410f295b1e2fa
Fyodor Sheremetyev <sheremetyev@gmail.com>
2012-05-02 21:41:17 +0400

    Implement trivial conversion from TextJSON to XeLaTeX.

--- bin/texts-cli.js
+++ bin/texts-cli.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('texts 0.0.1');
+var io = require('../lib/io');
+var xelatex = require('../lib/writer/xelatex');
+
+io.readInput(function (err, input) {
+  var text = JSON.parse(input);
+  var tex = xelatex(text);
+  io.writeOutput(tex);
+});

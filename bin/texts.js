23d7b9321893473d4c864da841c22f5b02384100
Fyodor Sheremetyev <sheremetyev@gmail.com>
2012-05-03 17:55:12 +0400

    Select source format via an option as well.

--- bin/texts.js
+++ bin/texts.js
@@ -3,10 +3,12 @@
 var nopt = require('nopt');
 var options = {
   'help': Boolean,
+  'from' : [ 'textjson' ],
   'to' : [ 'xelatex', 'html5' ],
 };
 var shorthands = {
   'h' : '--help',
+  'f' : '--from',
   't' : '--to',
 };
 var parsed = nopt(options, shorthands);
@@ -16,32 +18,35 @@ if (parsed.help) {
     'Usage: texts [OPTIONS] [FILES]',
     '',
     'Options:',
+    '  -f FORMAT             --from=FORMAT',
     '  -t FORMAT             --to=FORMAT',
     '  -h                    --help',
   ].join('\n'));
   process.exit(1);
 }
 
-parsed.to = parsed.to || 'html5';
+parsed.from = parsed.from || 'textjson';
+parsed.to   = parsed.to   || 'html5';
 
 // option parsing completed
 
 var io = require('../lib/io');
 var fs = require('fs');
+var reader = require('../lib/reader/' + parsed.from);
 var writer = require('../lib/writer/' + parsed.to);
 
 if (parsed.argv.remain.length) {
   // process files
   parsed.argv.remain.forEach(function(inputFile) {
     var input = fs.readFileSync(inputFile);
-    var text = JSON.parse(input);
+    var text = reader(input);
     var output = writer(text);
     io.writeOutput(output);
   });
 } else {
   // process stdin
   io.readInput(function (err, input) {
-    var text = JSON.parse(input);
+    var text = reader(input);
     var output = writer(text);
     io.writeOutput(output);
   });

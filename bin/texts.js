b8c39e379b574a99e0b97e37836b16ba5f28658b
Fyodor Sheremetyev <sheremetyev@gmail.com>
2012-05-03 17:51:33 +0400

    Parse target format option.

--- bin/texts.js
+++ bin/texts.js
@@ -1,11 +1,48 @@
 #!/usr/bin/env node
 
-var io = require('../lib/io');
-var xelatex = require('../lib/writer/xelatex');
-var html5 = require('../lib/writer/html5');
+var nopt = require('nopt');
+var options = {
+  'help': Boolean,
+  'to' : [ 'xelatex', 'html5' ],
+};
+var shorthands = {
+  'h' : '--help',
+  't' : '--to',
+};
+var parsed = nopt(options, shorthands);
 
-io.readInput(function (err, input) {
-  var text = JSON.parse(input);
-  var output = html5(text);
-  io.writeOutput(output);
-});
+if (parsed.help) {
+  console.log([
+    'Usage: texts [OPTIONS] [FILES]',
+    '',
+    'Options:',
+    '  -t FORMAT             --to=FORMAT',
+    '  -h                    --help',
+  ].join('\n'));
+  process.exit(1);
+}
+
+parsed.to = parsed.to || 'html5';
+
+// option parsing completed
+
+var io = require('../lib/io');
+var fs = require('fs');
+var writer = require('../lib/writer/' + parsed.to);
+
+if (parsed.argv.remain.length) {
+  // process files
+  parsed.argv.remain.forEach(function(inputFile) {
+    var input = fs.readFileSync(inputFile);
+    var text = JSON.parse(input);
+    var output = writer(text);
+    io.writeOutput(output);
+  });
+} else {
+  // process stdin
+  io.readInput(function (err, input) {
+    var text = JSON.parse(input);
+    var output = writer(text);
+    io.writeOutput(output);
+  });
+}
